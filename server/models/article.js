4893f211f068e8b5ba0740d3310d8a8586911367
Unknown <zwotawa@gmail.com>
2017-10-14 10:08:51 -0500

    article created

---
+++ server/models/article.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const articleSchema = mongoose.Schema({
+  url: {
+    type: String
+  },
+  editor: {
+    type: String
+  },
+  pubDate: {
+    type: Date
+  },
+  tags: [{
+    name: String,
+    count: Number,
+    ssr: Number
+  }],
+  domains: [{
+    type: String
+  }],
+  author: [{
+    name: String,
+    domain: String,
+    tags: String,
+    articles: String
+  }]
+})
+
+const Article = module.exports = mongoose.model('Article', articleSchema)
+
+module.exports.addArticle = function (newArticle, callback) {
+  newArticle.save(callback)
+}
+
+module.exports.updateArticle = function (_id, updatedArticle, callback) {
+  console.log(updatedArticle)
+  Article.findByIdAndUpdate({_id}, updatedArticle, callback)
+}
+
+module.exports.deleteArticle = function (_id, callback) {
+  Article.findByIdAndRemove(_id, callback)
+}
