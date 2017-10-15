ec0432797f71d14b43b655e53c3a57bd1fc6b5b2
Matt Bernardini <matt.bernardini@gmail.com>
2017-10-15 07:11:05 -0500

    added api documentation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -41,6 +41,37 @@ const router = new express.Router()
  *   ImV4cCI6MTUwODY3MTMyN30.fyTG6imgxHq7YlBOekzvPwi_kpT6AZ-XO7l0tT15RiU"
  *  }
  *  
+ * @apiError ValidationError The information submitted did not pass validation
+ * @apiErrorExample Error-Response:
+ *  HTTP/1.1 400 BAD REQUEST
+ *  {
+ *    "errors": [
+ *      {
+ *        "parameter": "username",
+ *        "message": "Please use only alphanumeric characters for username",
+ *        "value": "Ih@t3val1d@t1on"
+ *      },
+ *      {
+ *        "parameter": "email",
+ *        "message": "Please enter a valid email",
+ *        "value": "ihatevalidemails"
+ *      }
+ *    ]
+ *  }
+ * 
+ * @apiError InternalServerError The server encountered an error while processing the request
+ * @apiErrorExample Error-Response:
+ *  HTTP/1.1 500 INTERNAL SERVER ERROR
+ *  {
+ *    "errors": [
+ *      {
+ *        "parameter": " mu-hackathon-server.users.$username_1 dup key",
+ *        "message": "E11000 duplicate key error index",
+ *        "value": " \"DeathwingX\" }"
+ *      }
+ *    ]
+ *  }
+ * 
  * @apiExample [{application/json}] Signup
  *  {
  *    "username": "JohnDoe",
@@ -70,7 +101,7 @@ router.post('/signup', (req, res) => {
         console.log(error)
         erArray.errors.push(new errors.ErrorMessage(error.errmsg.split(':')[0],
           error.errmsg.split(':')[1], error.errmsg.split(':')[3]))
-        return res.status(500).json({error: erArray.errors})
+        return res.status(500).json({errors: [erArray.errors]})
       } else {
         // Add user and set token
         const user = prep.prepForSend(user2)
@@ -83,13 +114,88 @@ router.post('/signup', (req, res) => {
   })
 })
 
-// /login Api call taken in a json and if user exists it returns a token
+/**
+ * @api {post} /auth/login Request for login
+ * @apiSampleRequest https://mu-hackathon.herokuapp.com/auth/login
+ * @apiName Login
+ * @apiGroup Auth
+ * 
+ * @apiParam {String} username Username of user logging in
+ * @apiParam {String} password Password of the user logging in
+ * 
+ * @apiSuccess {String} username Username of the user
+ * @apiSuccess {String} email Email of the user
+ * @apiSuccess {String} id Mongo ID of the user
+ * @apiSuccess {Array} articles Articles the user has submitted tags for
+ * @apiSuccess {Token} jwt JWT token used in the controlling access
+ * 
+ * @apiSuccessExample Success-Response:
+ *  HTTP/1.1 200 SUCCESS
+ *  {
+ *    "user": {
+ *      "username": "JohnDoe",
+ *      "email": "valid@email.com",
+ *      "id": "59e211106617b361f022b1b6",
+ *      "articles": [
+ *        "59e2123b3514724de446d050",
+ *        "59e212cb84782e31a0208af5"
+ *      ]
+ *    },
+ *    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU5ZTM0NGRmZjExOWJkMWNjYzUwZjU1MiIsImFydGljbGVzIjpbX
+ *   SwidXNlcm5hbWUiOiJEZWF0aHdpbmdYIiwiZW1haWwiOiJtYXR0LmJlcm5hcmRpbmlAZ21haWwuY29tIiwiX192IjowLCJpYXQiOjE1MDgwNjY1Mjcs
+ *   ImV4cCI6MTUwODY3MTMyN30.fyTG6imgxHq7YlBOekzvPwi_kpT6AZ-XO7l0tT15RiU"
+ *  }
+ *  
+ * @apiError MissingInformationError The information required to login was not present
+ * @apiErrorExample Error-Response:
+ *  HTTP/1.1 400 BAD REQUEST
+ *  {
+ *    "errors": [
+ *      {
+ *        "message": "Missing required values",
+ *      }
+ *    ]
+ *  }
+ * 
+ * @apiError ValidationError The information submitted did not pass validation or was not found in the db
+ * @apiErrorExample Error-Response:
+ *  HTTP/1.1 401 UNAUTHORIZED
+ *  {
+ *    "errors": [
+ *      {
+ *        "message": "unauthorized",
+ *      }
+ *    ]
+ *  }
+ * 
+ * @apiError InternalServerError The server encountered an error while processing the request
+ * @apiErrorExample Error-Response:
+ *  HTTP/1.1 500 INTERNAL SERVER ERROR
+ *  {
+ *    "errors": [
+ *      {
+ *        "parameter": " mu-hackathon-server.users.$username_1 dup key",
+ *        "message": "E11000 duplicate key error index",
+ *        "value": " \"DeathwingX\" }"
+ *      }
+ *    ]
+ *  }
+ * 
+ * @apiExample [{application/json}] Signup
+ *  {
+ *    "username": "JohnDoe",
+ *    "password": "password"
+ *  }
+ */
 router.post('/login', (req, res) => {
   console.log(req.body)
+  let erArray = {}
   // Log username and password in the console
+  if (_.isEmpty(req.body.username) || _.isEmpty(req.body.password)) {
+    return res.status(400).json({errors: [{message: 'Missing required values'}]})
+  }
   const username = req.body.username
   const password = req.body.password
-  // console.log('username: ', username, 'password: ', password)
 
   // Check to see if the username exists
   User.getUserByUsername(username, (err, user2) => {
@@ -103,14 +209,19 @@ router.post('/login', (req, res) => {
       console.log('No User Found')
       // Set RFC 7235 401 header info
       return res.status(401).setHeader('WWW-Authenticate', 'Basic realm="CrowdCheck"')
-        .json({errors: {message: 'bad username'}})
+        .json({errors: {message: 'unauthorized'}})
     }
     // If the user does exist check the password
-    User.comparePassword(password, user2.password, (err, isMatch) => {
+    User.comparePassword(password, user2.password, (error, isMatch) => {
       // Lod where we are
       console.log('compare pass')
       // Handle error
-      if (err) throw err
+      if (!_.isEmpty(error)) {
+        console.log(error)
+        erArray.errors.push(new errors.ErrorMessage(error.errmsg.split(':')[0],
+          error.errmsg.split(':')[1], error.errmsg.split(':')[3]))
+        return res.status(500).json({errors: [erArray.errors]})
+      }
       // If it is a match
       if (isMatch) {
         // Create jwt token
@@ -122,8 +233,8 @@ router.post('/login', (req, res) => {
         return res.status(200).json({token, user})
       } else {
         // Set RFC 7235 401 header info
-        return res.status(401).setHeader('WWW-Authenticate', 'Basic realm="FASTCampus"')
-          .json({errors: {message: 'Bad password'}})
+        return res.status(401).setHeader('WWW-Authenticate', 'Basic realm="CrowdCheck"')
+          .json({errors: {message: 'unauthorized'}})
       }
     })
   })
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,7 @@ const MongoQS = require('mongo-querystring')
 const prep = require('../help/prepForSend')
 const _ = require('lodash')
 const val = require('../help/validation')
+const errors = require('../help/errors')
 const router = new express.Router()
 
 /*
@@ -66,8 +67,83 @@ router.patch('/', (req, res) => {
     })
   })
 })
-
+/**
+ * @api {get} /users Requests for accessing user data
+ * @apiSampleRequest https://mu-hackathon.herokuapp.com/users/:query
+ * @apiName GetUser
+ * @apiGroup User
+ * 
+ * @apiParam {String} query Mongo Query String of the information requested
+ * 
+ * @apiSuccess {String} username Username of the user
+ * @apiSuccess {String} email Email of the user
+ * @apiSuccess {String} id Mongo ID of the user
+ * @apiSuccess {Array} articles Articles the user has submitted tags for
+ * 
+ * @apiSuccessExample Success-Response:
+ *  HTTP/1.1 200 SUCCESS
+ *  {
+ *    "users": [
+ *      {
+ *        "username": "JohnDoe",
+ *        "email": "valid@email.com",
+ *        "id": "59e211106617b361f022b1b6",
+ *        "articles": [
+ *          "59e2123b3514724de446d050",
+ *          "59e212cb84782e31a0208af5"
+ *        ]
+ *      },
+ *      {
+ *        "username": "JaneDoe",
+ *        "email": "valid2@email.com",
+ *        "id": "59e211106617b361f022b1b6",
+ *        "articles": [
+ *          "59e2123b3514724de446d050",
+ *          "59e212cb84782e31a0208af5"
+ *        ]
+ *      }
+ *  }
+ *  
+ * @apiError MissingInformationError The information required to login was not present
+ * @apiErrorExample Error-Response:
+ *  HTTP/1.1 400 BAD REQUEST
+ *  {
+ *    "errors": [
+ *      {
+ *        "message": "Missing required values",
+ *      }
+ *    ]
+ *  }
+ * 
+ * @apiError ValidationError The information submitted did not pass validation or was not found in the db
+ * @apiErrorExample Error-Response:
+ *  HTTP/1.1 401 UNAUTHORIZED
+ *  {
+ *    "errors": [
+ *      {
+ *        "message": "unauthorized",
+ *      }
+ *    ]
+ *  }
+ * 
+ * @apiError InternalServerError The server encountered an error while processing the request
+ * @apiErrorExample Error-Response:
+ *  HTTP/1.1 500 INTERNAL SERVER ERROR
+ *  {
+ *    "errors": [
+ *      {
+ *        "parameter": " mu-hackathon-server.users.$username_1 dup key",
+ *        "message": "E11000 duplicate key error index",
+ *        "value": " \"DeathwingX\" }"
+ *      }
+ *    ]
+ *  }
+ * 
+ * @apiExample Example usage:
+ * curl -i https://mu-hackathon.herokuapp.com/users?email~=valid
+ */
 router.get('/', (req, res) => {
+  let erArray = {}
   console.log('GET /users')
   console.log(req.query)
   const qs = new MongoQS({
@@ -80,10 +156,15 @@ router.get('/', (req, res) => {
     }
   })
   const query = qs.parse(req.query)
-  User.find(query, (err, user2) => {
-    if (err) {
-      console.log(err)
-      return res.status(500).json({errors: {message: 'Error occured'}})
+  if (_.isEmpty(query)) {
+    return res.status(400).json({errors: [{message: 'missing required values'}]})
+  }
+  User.find(query, (error, user2) => {
+    if (!_.isEmpty(error)) {
+      console.log(error)
+      erArray.errors.push(new errors.ErrorMessage(error.errmsg.split(':')[0],
+        error.errmsg.split(':')[1], error.errmsg.split(':')[3]))
+      return res.status(500).json({errors: [erArray.errors]})
     }
     let users = []
     _.forEach(user2, function (u) {
